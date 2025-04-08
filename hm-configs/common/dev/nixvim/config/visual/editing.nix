eac7ffe45572a727859dc81a1879103ea9c14d61
mason <58895787+masoniis@users.noreply.github.com>
2025-04-07 22:00:43 -0600

    temp disable simons plugins, causing errors

--- hm-configs/common/dev/nixvim/config/visual/editing.nix
+++ hm-configs/common/dev/nixvim/config/visual/editing.nix
@@ -1,23 +1,23 @@
-# related to the visual editting experience!
+# related to the visual editing experience!
 { pkgs, ... }:
 {
   programs.nixvim = {
     plugins = {
       illuminate.enable = true;
       todo-comments.enable = true;
-      intellitab = {
-        enable = true;
-        # TODO: remove custom intellitab package once upstream accepts pr
-        package = pkgs.vimUtils.buildVimPlugin {
-          name = "intellitab";
-          src = pkgs.fetchFromGitHub {
-            owner = "simondoesstuff";
-            repo = "intellitab.nvim";
-            rev = "fad06f80b44186767eba348283c3b88827a969b1";
-            sha256 = "sha256-ShMECcqFQS607ZWRKHEh1cSLT/XChx5JpNOHSP3h/7I=";
-          };
-        };
-      };
+      # intellitab = {
+      #   enable = true;
+      #   # TODO: remove custom intellitab package once upstream accepts pr
+      #   package = pkgs.vimUtils.buildVimPlugin {
+      #     name = "intellitab";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "simondoesstuff";
+      #       repo = "intellitab.nvim";
+      #       rev = "fad06f80b44186767eba348283c3b88827a969b1";
+      #       sha256 = "sha256-ShMECcqFQS607ZWRKHEh1cSLT/XChx5JpNOHSP3h/7I=";
+      #     };
+      #   };
+      # };
       nvim-ufo.enable = true;
     };
 
