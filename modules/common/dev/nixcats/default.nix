dd8a979e0a8d09786a4e9940f51412d4240bf473
mason <58895787+masoniis@users.noreply.github.com>
2025-08-28 17:13:48 -0600

    glsl and zig

--- modules/common/dev/nixcats/default.nix
+++ modules/common/dev/nixcats/default.nix
@@ -22,13 +22,25 @@
 
           # Determines which plugin categories to enable
           categories = {
+            # INFO: ----------------
+            #    Broad categories
+            # ----------------------
             necessary = true;
+            general = true;
+
+            # INFO: -------------------
+            #    Language categories
+            # -------------------------
             lua.enable = true;
             java.enable = true;
             markdown.enable = true;
             python.enable = true;
-            general = true;
+            zig.enable = true;
+            glsl.enable = true;
 
+            # INFO: ----------------
+            #    Config variables
+            # ----------------------
             javaPaths = {
               java_debug_dir = "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server";
               java_test_dir = "${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server";
@@ -49,6 +61,10 @@
       }:
       {
         lspsAndRuntimeDeps = with pkgs; {
+          glsl = [
+            glsl_analyzer
+          ];
+
           java = [
             jdt-language-server
             vscode-extensions.vscjava.vscode-java-test
@@ -67,6 +83,10 @@
           python = [
             basedpyright
             ruff
+          ];
+
+          zig = [
+            zls # zig langserver
           ];
 
           general = [
