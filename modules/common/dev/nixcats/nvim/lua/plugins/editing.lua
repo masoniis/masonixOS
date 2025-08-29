9d1daff3390ab9762dbbe2b37c0845d32e531268
mason <58895787+masoniis@users.noreply.github.com>
2025-08-28 18:03:01 -0600

    barbar upgrades

--- modules/common/dev/nixcats/nvim/lua/plugins/editing.lua
+++ modules/common/dev/nixcats/nvim/lua/plugins/editing.lua
@@ -6,21 +6,46 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 		event = { "BufReadPre", "BufNewFile" },
-		init = function()
-			vim.g.barbar_auto_setup = false
-			vim.keymap.set("n", "<leader>bc", "<cmd>BufferClose!<cr>", { desc = "close buffer" })
-			vim.keymap.set("n", "<leader>bp", "<cmd>BufferPrevious<cr>", { desc = "previous buffer" })
-			vim.keymap.set("n", "<leader>bn", "<cmd>BufferNext<cr>", { desc = "next buffer" })
-		end,
 		opts = {
-			-- highlight_visible = false,
 			auto_hide = 1, -- Hide if 1 or less tabs
 			icons = {
 				separator = { left = "┃", right = "" },
 				separator_at_end = false,
+				button = false,
 			},
 			sidebar_filetypes = {
 				["neo-tree"] = { event = "BufWipeout", text = "file tree" },
+			},
+		},
+		keys = {
+			{
+				"-",
+				"<cmd>BufferClose!<cr>",
+				desc = "close buffer",
+			},
+			{
+				"<C-,>",
+				"<cmd>BufferPrevious<cr>",
+				mode = { "n", "t" },
+				desc = "previous buffer",
+			},
+			{
+				"<C-.>",
+				"<cmd>BufferNext<cr>",
+				mode = { "n", "t" },
+				desc = "next buffer",
+			},
+			{
+				"<C-S-.>",
+				"<cmd>BufferMoveNext<cr>",
+				mode = { "n", "t" },
+				desc = "move buffer to next",
+			},
+			{
+				"<C-S-,>",
+				"<cmd>BufferMovePrevious<cr>",
+				mode = { "n", "t" },
+				desc = "move buffer to previous",
 			},
 		},
 	},
