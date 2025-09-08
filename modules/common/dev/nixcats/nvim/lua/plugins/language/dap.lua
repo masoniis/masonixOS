f6cdb4eca280c41fb20e16434398152997440cf0
mason <58895787+masoniis@users.noreply.github.com>
2025-09-07 19:31:48 -0600

    fixed formatting of some lua files, improved overlay nix file readability

--- modules/common/dev/nixcats/nvim/lua/plugins/language/dap.lua
+++ modules/common/dev/nixcats/nvim/lua/plugins/language/dap.lua
@@ -3,17 +3,17 @@ return {
 		"mfussenegger/nvim-dap",
 		keys = {
 			{
-				nixCats('binds.debug.toggle_breakpoint'),
+				nixCats("binds.debug.toggle_breakpoint"),
 				"<cmd>lua require'dap'.toggle_breakpoint()<cr>",
 				desc = "toggle breakpoint",
 			},
 
-			{ nixCats('binds.debug.continue'), "<cmd>lua require'dap'.continue()<cr>", desc = "start/continue" },
-			{ nixCats('binds.debug.step_over'), "<cmd>lua require'dap'.step_over()<cr>", desc = "step over" },
-			{ nixCats('binds.debug.step_into'), "<cmd>lua require'dap'.step_into()<cr>", desc = "step into" },
+			{ nixCats("binds.debug.continue"), "<cmd>lua require'dap'.continue()<cr>", desc = "start/continue" },
+			{ nixCats("binds.debug.step_over"), "<cmd>lua require'dap'.step_over()<cr>", desc = "step over" },
+			{ nixCats("binds.debug.step_into"), "<cmd>lua require'dap'.step_into()<cr>", desc = "step into" },
 			-- Arrow keys for the above keybinds instead
-			{ nixCats('binds.debug.step_out'), "<cmd>lua require'dap'.step_out()<cr>", desc = "step out" },
-			{ nixCats('binds.debug.step_into_alt'), "<cmd>lua require'dap'.step_into()<cr>", desc = "step into" },
+			{ nixCats("binds.debug.step_out"), "<cmd>lua require'dap'.step_out()<cr>", desc = "step out" },
+			{ nixCats("binds.debug.step_into_alt"), "<cmd>lua require'dap'.step_into()<cr>", desc = "step into" },
 		},
 	},
 	{
@@ -21,7 +21,7 @@ return {
 		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
 		opts = {},
 		keys = {
-			{ nixCats('binds.debug.toggle_ui'), "<cmd>lua require'dapui'.toggle()<cr>", desc = "toggle dap ui" },
+			{ nixCats("binds.debug.toggle_ui"), "<cmd>lua require'dapui'.toggle()<cr>", desc = "toggle dap ui" },
 		},
 	},
 
--- overlays/masonpkgs/default.nix
+++ overlays/masonpkgs/default.nix
@@ -3,7 +3,10 @@ self: super:
 let
   lib = super.lib;
 
-  # Read plugin files from relative dir
+  # INFO: --------------------------------------------------------------
+  #         Auto read nvim-plugins dir to an attr set for overlay
+  # --------------------------------------------------------------------
+
   pluginsPath = ./nvim-plugins;
   pluginFiles = lib.attrNames (
     lib.filterAttrs (name: type: type == "regular" && lib.strings.hasSuffix ".nix" name) (
@@ -11,20 +14,18 @@ let
     )
   );
 
-  # Create a custom plugins attr set
   customVimPlugins = lib.listToAttrs (
     lib.map (file: {
-      # The key for the attr set, e.g., "slimline.nix" -> "slimline-masonpkgs"
+      # plugin name for the vim plugin overlay is based on file name + masonpkgs
+      # "slimline.nix" -> "slimline-masonpkgs"
       name = "${lib.strings.removeSuffix ".nix" file}-masonpkgs";
-      # The value for the attr set, e.g., callPackage ./nvim-plugins/slimline.nix {}
       value = super.callPackage (pluginsPath + "/${file}") { };
     }) pluginFiles
   );
-
 in
 {
-  lobster = args: super.callPackage ./lobster/default.nix args;
   jerry = args: super.callPackage ./jerry/default.nix args;
+  lobster = args: super.callPackage ./lobster/default.nix args;
   timetrack = args: super.callPackage ./timetrack/default.nix args;
 
   vimPlugins = super.vimPlugins // customVimPlugins;
