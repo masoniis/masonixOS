c8554828f7ab933e05d9f902cdb3c9054e3f814e
mason <58895787+masoniis@users.noreply.github.com>
2025-12-16 05:01:22 -0700

    add dua to essential system packages

--- modules/common/system/essentials.nix
+++ modules/common/system/essentials.nix
@@ -8,6 +8,7 @@
   home.packages = with pkgs; [
     neofetch # eblic show system stuff
     fzf # useful for piping commands into searches
+    dua # disk usage analyzer tool
 
     # font stuff
     fontconfig
