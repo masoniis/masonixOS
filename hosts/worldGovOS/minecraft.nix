7e14ed8146bdabfd875e579740d2dc0e2ba3e048
mason <58895787+masoniis@users.noreply.github.com>
2025-12-19 15:44:53 -0700

    worldgov: creative astral server

--- hosts/worldGovOS/minecraft.nix
+++ hosts/worldGovOS/minecraft.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
-  networking.firewall.allowedTCPPorts = [ 25565 ];
+  networking.firewall.allowedTCPPorts = [
+    25565
+    25566
+  ];
 
   users.users.minecraft = {
     isSystemUser = true;
@@ -10,8 +13,8 @@
   };
   users.groups.minecraft = { };
 
-  systemd.services.create-astral = {
-    description = "Create: Astral Server";
+  systemd.services.astral = {
+    description = "Create Astral Server";
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
 
@@ -23,6 +26,32 @@
       Group = "minecraft";
       WorkingDirectory = "/data/minecraft/astral";
       StateDirectory = "minecraft/astral";
+
+      # Startup/restart handling
+      TimeoutStopSec = "1500"; # give ~15 minutes before force killing on a shutdown
+      Restart = "on-failure";
+      RestartSec = "20s";
+
+      # Security
+      ProtectSystem = "full";
+      PrivateTmp = true;
+      ProtectHome = true;
+    };
+  };
+
+  systemd.services.astral-creative = {
+    description = "Create Astral Server (creative)";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.jdk17}/bin/java -Xmx6G -Xms4G -jar server.jar nogui";
+
+      # Data
+      User = "minecraft";
+      Group = "minecraft";
+      WorkingDirectory = "/data/minecraft/astral-creative";
+      StateDirectory = "minecraft/astral-creative";
 
       # Startup/restart handling
       TimeoutStopSec = "1500"; # give ~15 minutes before force killing on a shutdown
