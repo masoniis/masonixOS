3c95076ec0f32d7bdda0ddc017208ba99ec6a9dd
mason <58895787+masoniis@users.noreply.github.com>
2025-12-19 20:08:40 -0700

    all systems: neofetch -> fastfetch

--- modules/common/system/essentials.nix
+++ modules/common/system/essentials.nix
@@ -6,7 +6,7 @@
 }:
 {
   home.packages = with pkgs; [
-    neofetch # eblic show system stuff
+    fastfetch # eblic show system stuff
     fzf # useful for piping commands into searches
     dua # disk usage analyzer tool
 
