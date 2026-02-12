db7dbfdd760d5167f0a265de33f84d0de63f17f7
mason <58895787+masoniis@users.noreply.github.com>
2026-02-12 09:33:27 -0700

    fix git settings

--- modules/common/dev/git.nix
+++ modules/common/dev/git.nix
@@ -11,9 +11,11 @@
 
   programs.git = {
     enable = true;
-    userName = "mason";
-    userEmail = "58895787+masoniis@users.noreply.github.com";
-    extraConfig = {
+    settings = {
+      user = {
+        name = "mason";
+        email = "58895787+masoniis@users.noreply.github.com";
+      };
       merge.tool = "nvimdiff2";
       init.defaultBranch = "main";
       pull.rebase = false; # default to merging
